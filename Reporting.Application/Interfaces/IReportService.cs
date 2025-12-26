2a386f2f4b2004592342bd450ff366d79e260efc
Hosna <h.m.hosna@gmail.com>
2025-12-26 11:50:10 +0330

    refactor: report service updated

--- Reporting.Application/Interfaces/IReportService.cs
+++ Reporting.Application/Interfaces/IReportService.cs
@@ -1,6 +1,11 @@
+using Reporting.Application.DTOs;
 namespace Reporting.Application.Interfaces;
 
 public interface IReportService
 {
-
+    Task<List<TasksPerUserReportDto>> GetTasksPerUserAsync(
+        bool useRawSql = false,
+        string? status = null,
+        DateTime? from = null,
+        DateTime? to = null);
 }
