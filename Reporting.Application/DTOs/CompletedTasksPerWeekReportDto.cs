1b2b0477df4dfa3dad86308b68f29451a00c0124
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-12-27 23:09:22 +0330

    add dto for completed tasks per week report

---
+++ Reporting.Application/DTOs/CompletedTasksPerWeekReportDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reporting.Application.DTOs
+{
+    public class CompletedTasksPerWeekReportDto
+    {
+        public int Year { get; set; }
+        public int WeekNumber { get; set; }
+        public int CompletedTasksCount { get; set; }
+    }
+}
