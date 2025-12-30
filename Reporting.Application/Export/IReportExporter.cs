0635797ab121b712fe97c227ee47bc4a1112e829
Hosna <h.m.hosna@gmail.com>
2025-12-30 22:27:27 +0330

    fix: delete extra classes

--- Reporting.Application/Export/IReportExporter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Reporting.Application.Interfaces;
-
-public interface IReportExporter
-{
-    string ReportType { get; }
-
-    Task<byte[]> ExportAsync(HttpContext http, CancellationToken ct);
-}
-
