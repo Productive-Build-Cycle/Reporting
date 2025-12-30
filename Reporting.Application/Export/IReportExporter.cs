dbd860c9d2079b2f7c19257d89f3bf7050279fb1
Hosna <h.m.hosna@gmail.com>
2025-12-30 21:16:43 +0330

    feat(export): add IReportExporter contract for pluggable report exporters

---
+++ Reporting.Application/Export/IReportExporter.cs
@@ -0,0 +1,9 @@
+﻿namespace Reporting.Application.Interfaces;
+
+public interface IReportExporter
+{
+    string ReportType { get; }
+
+    Task<byte[]> ExportAsync(HttpContext http, CancellationToken ct);
+}
+
