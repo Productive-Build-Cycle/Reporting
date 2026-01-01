b0a3dc4ac7be26ad6857173cadfa4a5cba02279e
Hosna <h.m.hosna@gmail.com>
2026-01-01 23:46:23 +0330

    refactor: interface of the repository updated to GetTasksPerUser_DapperAsync

--- Reporting.Application/Interfaces/IRepository.cs
+++ Reporting.Application/Interfaces/IRepository.cs
@@ -4,21 +4,10 @@ namespace Reporting.Application.Interfaces;
 
 public interface IReportRepository
 {
-    Task<List<TasksPerUserReportDto>> GetTasksPerUser_LinqAsync(
-        string? status = null,
-        DateTime? from = null,
-        DateTime? to = null);
-
-    Task<List<TasksPerUserReportDto>> GetTasksPerUser_RawSqlAsync(
-        string? status = null,
-        DateTime? from = null,
-        DateTime? to = null);
-
-    Task<List<TasksPerUserReportDto>> GetTasksPerUser_SpAsync(
+    Task<List<TasksPerUserReportDto>> GetTasksPerUser_DapperAsync(
     string? status = null,
     DateTime? from = null,
-    DateTime? to = null,
-    int? teamId = null);
+    DateTime? to = null);
 
 
     Task<List<CompletedTasksPerWeekReportDto>> GetCompletedTasksPerWeekAsync(
