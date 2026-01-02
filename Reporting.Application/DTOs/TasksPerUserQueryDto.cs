aac75cda4f913b9a496629af9a8642cbffc9efd0
Hosna <h.m.hosna@gmail.com>
2026-01-02 21:13:33 +0330

    refactor: some changes require to query dto

--- Reporting.Application/DTOs/TasksPerUserQueryDto.cs
+++ Reporting.Application/DTOs/TasksPerUserQueryDto.cs
@@ -3,8 +3,8 @@
 public class TasksPerUserQueryDto
 {
     public string? Status { get; set; } 
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
+    public DateTime? From { get; set; }
+    public DateTime? To { get; set; }
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 10;
 }
